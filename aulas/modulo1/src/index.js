c9abf79da51509c08399540dd58b5d78b65bfff7
Edison Ferraz <edisonferraz@gmail.com>
2018-12-08 00:25:57 +0100

    conceitos

--- aulas/modulo1/src/index.js
+++ aulas/modulo1/src/index.js
@@ -1,3 +1,33 @@
-const soma = (a, b) => a + b;
+import React, { Component, Fragment } from 'react';
+import { render } from 'react-dom';
 
-alert(soma(5, 3));
+import Button from './Button';
+
+class App extends Component {
+  state = {
+    counter: 0,
+  };
+
+  componentDidMount = () => {
+    console.log('componentDidMount');
+  };
+
+  handleClick = () => {
+    const { counter } = this.state;
+    this.setState({ counter: counter + 1 });
+  };
+
+  render() {
+    const { counter } = this.state;
+
+    return (
+      <Fragment>
+        <h1>Hello</h1>
+        <h2>{counter}</h2>
+        <Button onClick={this.handleClick}>Somar</Button>
+      </Fragment>
+    );
+  }
+}
+
+render(<App />, document.getElementById('app'));
