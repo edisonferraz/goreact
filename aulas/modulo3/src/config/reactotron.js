c02bfda462dd647fc391eb5e790845dd467fe30f
Edison Ferraz <edisonferraz@gmail.com>
2018-12-13 19:02:19 +0100

    Plugin do saga no Reactotron

--- aulas/modulo3/src/config/reactotron.js
+++ aulas/modulo3/src/config/reactotron.js
@@ -1,9 +1,11 @@
 import Reactrotron from 'reactotron-react-js';
 import { reactotronRedux } from 'reactotron-redux';
+import sagaPlugin from 'reactotron-redux-saga';
 
 if (process.env.NODE_ENV === 'development') {
   const tron = Reactrotron.configure()
     .use(reactotronRedux())
+    .use(sagaPlugin())
     .connect();
 
   tron.clear();
